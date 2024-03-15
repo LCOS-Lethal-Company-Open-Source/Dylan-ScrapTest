0bb6298ead9cabe62c2d8c2025fedcff0494ec12
Floofer++ <56936160+dylanrafael05@users.noreply.github.com>
2024-03-15 17:06:46 -0400

    I did it

--- Plugin.cs
+++ Plugin.cs
@@ -1,13 +1,22 @@
-﻿using BepInEx;
+﻿using System.IO;
+using System.Reflection;
+using BepInEx;
+using LethalLib.Modules;
+using UnityEngine;
 namespace Template;
 
-[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION), BepInDependency(LethalLib.Plugin.ModGUID)]
 public class Plugin : BaseUnityPlugin
 {
     private void Awake()
     {
-        // Plugin load logic goes here!
-        // This script acts like a unity object.
-        Logger.LogInfo($"Hello World!");
+        var assets = AssetBundle.LoadFromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream($"{PluginInfo.PLUGIN_GUID}.assets"));
+
+        Item donut = assets.LoadAsset<Item>("Assets/Prefabs/Donut - Lilu/DonutLilu.asset");
+        Utilities.FixMixerGroups(donut.spawnPrefab);
+        NetworkPrefabs.RegisterNetworkPrefab(donut.spawnPrefab);
+        Items.RegisterScrap(donut, 1000, Levels.LevelTypes.All);
+
+        Logger.LogInfo("Donuts loaded!");
     }
 }
